187cb245c276052478a40489f1e661e67df04806
kokaine <58642970+kokaine@users.noreply.github.com>
2019-12-09 19:43:38 -0500

    Update helloclass.cpp

--- helloclass.cpp
+++ helloclass.cpp
@@ -2,10 +2,18 @@ using namespace std;
  
 int main() 
 { 
+ 
+ /* John's section */
  cout << "Make edits";
     cout<<"Hello Class"; 
 
     cout<<"Howdy y'all -jv";
+ 
+ /* Koki's section */
+ 
+ 
+ /*Wang's section */
+ 
  
 
   
